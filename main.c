02b1dfdf642400106888798c51c1c9b839a6146a
jooyeon-1031 <jmhjoin@naver.com>
2022-09-20 17:23:27 +0900

    04

--- main.c
+++ main.c
@@ -4,11 +4,12 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	char text;
+	int a,b;
+	printf("input two integers:");
+
+	scanf("%d %d", &a, &b);
 	
-	printf("input a character:");
-	scanf("%c", &text);
+	printf("result is %f\n", (float)a/b);
 	
-	printf("the next character of %c (%d) is %c (%d)\n", text, text, text+1, text+1);
 	return 0;
 }
